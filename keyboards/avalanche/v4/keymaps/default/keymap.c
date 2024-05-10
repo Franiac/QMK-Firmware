6345a4d357d48239e2966d23da48ee6cd1381a06
Franiac <d.rebitzer@gmx.de>
2024-05-10 13:22:41 +0200

    Del and Backspace on single key

--- keyboards/avalanche/v4/keymaps/default/keymap.c
+++ keyboards/avalanche/v4/keymaps/default/keymap.c
@@ -13,8 +13,9 @@ enum layer {
     LAYER_2,
 };
 
-#define MO1 MO(LAYER_1)
+#define LT1 LT(LAYER_1, KC_BSPC)
 #define MO2 MO(LAYER_2)
+#define ALT_DEL LALT_T(KC_DEL)
 #define SFT_SPC LSFT_T(KC_SPC)
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
@@ -23,19 +24,19 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                  KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                                                 KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_GRV,
         KC_CAPS, KC_LSFT, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                                                 KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_BSLS,
                  KC_LCTL, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_NO,   KC_NO,            KC_NO,   KC_NO,   KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RCTL,
-                                            KC_NUM,  KC_LALT, KC_LGUI, SFT_SPC, KC_ESC,           KC_ENT,  MO1,     MO2,     KC_RALT, KC_APP
+                                            KC_NUM,  KC_LGUI, ALT_DEL, SFT_SPC, KC_ESC,           KC_ENT,  LT1,     MO2,     KC_RALT, KC_APP
     ),
     [LAYER_1] = LAYOUT(
                  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                              KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                  KC_TRNS, KC_0,    KC_1,    KC_2,    KC_3,    KC_LPRN,                                              KC_RPRN, KC_HOME, KC_UP,   KC_END,  KC_NO,   KC_NO,
-        KC_TRNS, KC_TRNS, KC_0,    KC_4,    KC_5,    KC_6,    KC_DEL,                                               KC_BSPC, KC_LEFT, KC_DOWN, KC_RGHT, KC_MINS, KC_EQL,  KC_NO,
-                 KC_TRNS, KC_0,    KC_7,    KC_8,    KC_9,    KC_LCBR, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, KC_RCBR, KC_PGDN, KC_INS,  KC_PGUP, KC_NO,   KC_TRNS,
+        KC_TRNS, KC_TRNS, KC_0,    KC_4,    KC_5,    KC_6,    KC_LCBR,                                              KC_RCBR, KC_LEFT, KC_DOWN, KC_RGHT, KC_MINS, KC_EQL,  KC_NO,
+                 KC_TRNS, KC_0,    KC_7,    KC_8,    KC_9,    KC_LBRC, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, KC_RBRC, KC_PGDN, KC_INS,  KC_PGUP, KC_NO,   KC_TRNS,
                                             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     ),
     [LAYER_2] = LAYOUT(
                  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                                              KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,
                  KC_TRNS, KC_P0,   KC_P1,   KC_P2,   KC_P3,   KC_LPRN,                                              KC_RPRN, KC_EXLM, KC_AT,   KC_HASH, UE,      SZ,
-        KC_TRNS, KC_TRNS, KC_P0,   KC_P4,   KC_P5,   KC_P6,   KC_DEL,                                               KC_BSPC, KC_DLR,  KC_PERC, KC_CIRC, OE,      AE,      KC_NO,
+        KC_TRNS, KC_TRNS, KC_P0,   KC_P4,   KC_P5,   KC_P6,   KC_LCBR,                                              KC_RCBR, KC_DLR,  KC_PERC, KC_CIRC, OE,      AE,      KC_NO,
                  KC_TRNS, KC_P0,   KC_P7,   KC_P8,   KC_P9,   KC_LBRC, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, KC_RBRC, KC_AMPR, KC_ASTR, KC_NO,   KC_NO,   KC_TRNS,
                                             KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,          KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
     )
